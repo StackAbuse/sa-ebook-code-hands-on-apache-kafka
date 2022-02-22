1d815daa22fa67be01b80d8c500f5467381ad1ff
Arpendu Kumar Garai <Arpendu.KumarGarai@microfocus.com>
2022-02-22 13:12:16 +0530

    code refactor

--- Kafka-Consumer-API/src/main/java/com/stackabuse/consumer/NotificationDeserializer.java
+++ Kafka-Consumer-API/src/main/java/com/stackabuse/consumer/NotificationDeserializer.java
@@ -15,22 +15,14 @@ import java.util.Objects;
 public class NotificationDeserializer implements Deserializer<Notification> {
 
     private static final Logger logger = LoggerFactory.getLogger(NotificationDeserializer.class);
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @Override
     public Notification deserialize(String topic, byte[] data) {
-//        try {
-//            Notification test = Notification.fromJson(new String(data, StandardCharsets.UTF_8));
-//            logger.info("After deserializing... {}", test);
-//        } catch (JsonProcessingException e) {
-//            e.printStackTrace();
-//        }
         try {
             if (Objects.isNull(data)) {
                 logger.error("Received null while deserializing byte[]");
                 return null;
             }
-//            logger.info("Deserializing... {}", new String(data, StandardCharsets.UTF_8));
             return Notification.fromJson(new String(data, StandardCharsets.UTF_8));
         } catch (Exception e) {
             throw new SerializationException("Error when deserializing byte[] to Notification");
